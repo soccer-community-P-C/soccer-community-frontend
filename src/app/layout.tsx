969689d4da4712b9056b957ec6e4b0567e877294
PARKmaker <tkdgus8903@gmail.com>
2024-05-15 04:54:10 +0900

    Fix: 코드 컨벤션 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import MainHeader from '@/components/MainHeader/MainHeader';
 import localFont from 'next/font/local';
+import MainHeader from '@/components/MainHeader/MainHeader';
 
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: '해외축구 커뮤니티',
