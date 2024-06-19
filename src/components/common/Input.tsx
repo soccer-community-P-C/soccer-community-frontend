6ba714f3c95de7b65c9e65be9eaa0dfd45180aea
Chuhj <chj9574@naver.com>
2024-06-19 21:30:04 +0900

    Refactor: Input 컴포넌트 twMerge 사용하도록 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,14 +1,19 @@
+import { twMerge } from 'tailwind-merge';
+
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export default function Input({ ...props }: InputProps) {
+export default function Input({ className, ...props }: InputProps) {
   return (
     <input
-      className={`
+      className={twMerge(
+        `
         h-10 w-full rounded-md px-3 py-2
         focus-visible:outline-none focus-visible:ring-2
         focus-visible:ring-black focus-visible:ring-offset-2
         disabled:cursor-not-allowed disabled:opacity-50
-       `}
+       `,
+        className,
+      )}
       {...props}
     />
   );
