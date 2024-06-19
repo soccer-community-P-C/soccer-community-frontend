4ca0dd0aff2e501029c6d81f0432c710f4ac6302
Chuhj <chj9574@naver.com>
2024-06-19 21:30:52 +0900

    Feat: useInput 훅 추가

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,17 @@
+import { useCallback, useState } from 'react';
+
+export default function useInput(initialValue = '') {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = useCallback((input: string | React.ChangeEvent<HTMLInputElement>) => {
+    if (typeof input === 'string') {
+      setValue(input);
+    } else {
+      setValue(input.target.value);
+    }
+  }, []);
+
+  const clear = useCallback(() => setValue(''), []);
+
+  return { value, onChange: handleChange, clear };
+}
