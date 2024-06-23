97273f08a4e60dd0679a24346a173779dd4dff23
Chuhj <chj9574@naver.com>
2024-06-24 02:48:01 +0900

    Fix: fallback rewrites만 실행하도록 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
-import path from 'node:path';
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: `${process.env.API_URL}/:path*`,
-      },
-    ];
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `${process.env.API_URL}/:path*`,
+        },
+      ],
+    };
   },
 };
 
