ce73ca7bde81e0fc0c6d4acadf0b118a61d2596b
Chuhj <chj9574@naver.com>
2024-08-14 23:12:33 +0900

    Feat: 로그인 폼에 react-hook-form 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,62 @@
+'use client';
+
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 import Input from '@/components/common/input';
+import ErrorMessage from '@/components/auth-form/error-message';
+import Label from '@/components/auth-form/label';
+
+type TLoginInputs = {
+  email: string;
+  password: string;
+};
 
 export default function LogInPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TLoginInputs>({
+    mode: 'onSubmit',
+    defaultValues: { email: '', password: '' },
+  });
+
+  function onLogin(data: TLoginInputs) {
+    console.log(data);
+  }
+
   return (
-    <form className="mx-auto flex max-w-sm flex-col gap-5 pt-16">
+    <form className="mx-auto flex max-w-sm flex-col gap-5 pt-16" onSubmit={handleSubmit(onLogin)}>
       <span className="text-center text-3xl font-bold">로그인</span>
       <div className="flex flex-col gap-2">
-        <label htmlFor="email">이메일</label>
-        <Input id="email" required type="email" />
+        <Label error={errors.email} htmlFor="email">
+          이메일
+        </Label>
+        <Input
+          id="email"
+          type="email"
+          {...register('email', {
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: '올바른 이메일 형식이 아닙니다.',
+            },
+            required: true,
+          })}
+        />
+        {errors.email ? <ErrorMessage>{errors.email.message}</ErrorMessage> : null}
 
-        <label htmlFor="password">비밀번호</label>
-        <Input id="password" required type="password" />
+        <Label error={errors.password} htmlFor="password">
+          비밀번호
+        </Label>
+        <Input
+          id="password"
+          type="password"
+          {...register('password', {
+            required: true,
+            minLength: { value: 8, message: '8자 이상 입력해주세요.' },
+          })}
+        />
+        {errors.password ? <ErrorMessage>{errors.password.message}</ErrorMessage> : null}
       </div>
       <button className="rounded-md bg-zinc-600 py-3 text-white" type="submit">
         로그인
