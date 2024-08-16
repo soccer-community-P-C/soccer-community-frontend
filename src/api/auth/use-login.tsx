a3045fa6d3b2a7ab07231ea0c2ae9f823706fe7f
Chuhj <chj9574@naver.com>
2024-08-17 08:08:15 +0900

    Feat: 로그인 성공 시 토큰, 헤더, 로그인 설정 추가

--- src/api/auth/use-login.tsx
+++ src/api/auth/use-login.tsx
@@ -1,18 +1,23 @@
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import useLocalStorage from '@/hooks/use-local-storage';
+import { useAuth } from '@/contexts/auth-context';
 import { login } from '@/api/auth';
+import { setAuthHeader } from '@/api/auth/auths';
 import { TLoginInputs } from '@/types/auth';
 import { TOKEN_KEY } from '@/constants/auth';
 
 export function useLogin() {
-  const [, setToken] = useLocalStorage<string>(TOKEN_KEY, '');
   const router = useRouter();
+  const [, setToken] = useLocalStorage<string>(TOKEN_KEY, '');
+  const { setIsAuthenticated } = useAuth();
 
   return useMutation({
     mutationFn: (loginInputs: TLoginInputs) => login(loginInputs),
-    onSuccess: (data) => {
-      setToken(data.accessToken);
+    onSuccess: ({ accessToken }) => {
+      setToken(accessToken);
+      setAuthHeader(accessToken);
+      setIsAuthenticated(true);
       router.push('/');
     },
     onError: () => {},
