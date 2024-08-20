300bb350d0ffe99795bba63028e02fe64b98a81b
PARKmaker <tkdgus8903@gmail.com>
2024-08-20 22:15:14 +0900

    Style: min 크기 tailwindCss extend로 관리

--- src/components/main-header/main-header.tsx
+++ src/components/main-header/main-header.tsx
@@ -3,7 +3,7 @@ import MobileNav from '@/components/main-header/mobile-nav';
 
 export default function MainHeader() {
   return (
-    <header className="sticky top-0 z-40 h-[4rem] w-full bg-gray-600 duration-500">
+    <header className="sticky top-0 z-40 h-[4rem] w-full min-w-mobile bg-gray-600 duration-500">
       <MainNav />
       <MobileNav />
     </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       screens: {
         'sm-in': { max: '639px' },
       },
+      minWidth: {
+        mobile: '320px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
