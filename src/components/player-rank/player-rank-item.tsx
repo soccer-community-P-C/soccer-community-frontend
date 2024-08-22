27c95bfb90ef6f88bd81a5798af55553bea1b964
PARKmaker <tkdgus8903@gmail.com>
2024-08-22 21:18:04 +0900

    Design: 순위 행 크기 동일하게 수정

--- src/components/player-rank/player-rank-item.tsx
+++ src/components/player-rank/player-rank-item.tsx
@@ -1,11 +1,11 @@
 export default function PlayerRankItem() {
   return (
-    <tr className="h-[40px] border-b *:pl-2 even:bg-gray-100">
+    <tr className="h-8 border-b *:pl-2 even:bg-gray-100">
       <td>1</td>
       <td className="truncate text-left">
         <strong>엘랑 홀란</strong>
       </td>
-      <td className="truncate text-left text-xs">맨시티</td>
+      <td className="truncate text-left font-semibold">맨시티</td>
       <td>31</td>
       <td>31</td>
       <td>31</td>
