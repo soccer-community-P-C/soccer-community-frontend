be8d36b3fa9b8bd84aaecde36f269d492d4a661f
PARKmaker <tkdgus8903@gmail.com>
2024-08-29 01:10:59 +0900

    Comment: instance 토큰 설정 원상 복귀

--- src/api/intance.ts
+++ src/api/intance.ts
@@ -1,14 +1,5 @@
-// import axios from 'axios';
-//
-// export const instance = axios.create({
-//   baseURL: '/api',
-// });
-
 import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: '/api',
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
-  },
 });
