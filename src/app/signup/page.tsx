1e52a42938614610d1ff8ab815017cf6ab99643b
PARKmaker <tkdgus8903@gmail.com>
2024-08-29 00:51:27 +0900

    Feat: 회원가입 페이지 Box 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useForm } from 'react-hook-form';
+import Link from 'next/link';
 import Button from '@/components/common/button';
 import Input from '@/components/common/input';
 import ErrorMessage from '@/components/auth-form/error-message';
@@ -8,6 +9,8 @@ import Label from '@/components/auth-form/label';
 import { LoadingSpinner } from '@/components/common/loading-spinner';
 import { useSignup } from '@/api/auth/use-signup';
 import { TSignupInputs } from '@/types/auth';
+import Box from '@/components/common/box';
+import BoxHeading from '@/components/common/box-heading';
 
 export default function SignUpPage() {
   const {
@@ -26,64 +29,74 @@ export default function SignUpPage() {
   }
 
   return (
-    <form className="mx-auto flex max-w-sm flex-col gap-5 pt-16" onSubmit={handleSubmit(onSubmit)}>
-      <span className="text-center text-3xl font-bold">회원가입</span>
-      <div className="flex flex-col gap-2">
-        {/* TODO: Label, Input ErrorMessage를 한 컴포넌트로 분리 */}
-        <Label error={errors.email} htmlFor="email">
-          이메일
-        </Label>
-        <Input
-          id="email"
-          type="email"
-          {...register('email', {
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: '올바른 이메일 형식이 아닙니다.',
-            },
-            required: true,
-          })}
-        />
-        {errors.email ? <ErrorMessage>{errors.email.message}</ErrorMessage> : null}
+    <Box className="mx-auto mt-6 min-w-mobile max-w-md sm:mt-14">
+      <form className="mx-auto flex w-full flex-col gap-5" onSubmit={handleSubmit(onSubmit)}>
+        <BoxHeading className="text-center font-bold text-black" hTagType="h2">
+          회원가입
+        </BoxHeading>
+        <div className="flex flex-col gap-2">
+          {/* TODO: Label, Input ErrorMessage를 한 컴포넌트로 분리 */}
+          <Label error={errors.email} htmlFor="email">
+            이메일
+          </Label>
+          <Input
+            id="email"
+            type="email"
+            {...register('email', {
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: '올바른 이메일 형식이 아닙니다.',
+              },
+              required: true,
+            })}
+          />
+          {errors.email ? <ErrorMessage>{errors.email.message}</ErrorMessage> : null}
 
-        <Label error={errors.password} htmlFor="password">
-          비밀번호
-        </Label>
-        <Input
-          id="password"
-          type="password"
-          {...register('password', {
-            required: true,
-            minLength: { value: 8, message: '8자 이상 입력해주세요.' },
-          })}
-        />
-        {errors.password ? <ErrorMessage>{errors.password.message}</ErrorMessage> : null}
+          <Label error={errors.password} htmlFor="password">
+            비밀번호
+          </Label>
+          <Input
+            id="password"
+            type="password"
+            {...register('password', {
+              required: true,
+              minLength: { value: 8, message: '8자 이상 입력해주세요.' },
+            })}
+          />
+          {errors.password ? <ErrorMessage>{errors.password.message}</ErrorMessage> : null}
 
-        <Label error={errors.confirmPassword} htmlFor="confirm_password">
-          비밀번호 확인
-        </Label>
-        <Input
-          id="confirm_password"
-          type="password"
-          {...register('confirmPassword', {
-            required: true,
-            minLength: { value: 8, message: '8자 이상 입력해주세요.' },
-            validate: (confirmPassword) =>
-              getValues('password') === confirmPassword ? true : '비밀번호가 일치하지 않습니다.',
-          })}
-        />
-        {errors.confirmPassword ? (
-          <ErrorMessage>{errors.confirmPassword.message}</ErrorMessage>
-        ) : null}
+          <Label error={errors.confirmPassword} htmlFor="confirm_password">
+            비밀번호 확인
+          </Label>
+          <Input
+            id="confirm_password"
+            type="password"
+            {...register('confirmPassword', {
+              required: true,
+              minLength: { value: 8, message: '8자 이상 입력해주세요.' },
+              validate: (confirmPassword) =>
+                getValues('password') === confirmPassword ? true : '비밀번호가 일치하지 않습니다.',
+            })}
+          />
+          {errors.confirmPassword ? (
+            <ErrorMessage>{errors.confirmPassword.message}</ErrorMessage>
+          ) : null}
 
-        <Label error={errors.nickname} htmlFor="nickname">
-          닉네임
-        </Label>
-        <Input id="nickname" type="text" {...register('nickname', { required: true })} />
-      </div>
-      <Button disabled={isPending || isSuccess} type="submit">
-        {isPending || isSuccess ? <LoadingSpinner /> : '회원가입'}
-      </Button>
-    </form>
+          <Label error={errors.nickname} htmlFor="nickname">
+            닉네임
+          </Label>
+          <Input id="nickname" type="text" {...register('nickname', { required: true })} />
+        </div>
+        <Button disabled={isPending || isSuccess} type="submit">
+          {isPending || isSuccess ? <LoadingSpinner /> : '회원가입'}
+        </Button>
+        <div className="flex justify-end gap-2">
+          계정이 있으신가요?
+          <Link className="text-right font-bold text-blue-700 underline" href="/login">
+            로그인
+          </Link>
+        </div>
+      </form>
+    </Box>
   );
 }
