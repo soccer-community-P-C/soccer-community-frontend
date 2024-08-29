724693c296abd98e3c8da7ae4ddef65cb2e56670
PARKmaker <tkdgus8903@gmail.com>
2024-08-29 21:01:38 +0900

    Feat: 팀 순위, 선수 순위 시즌 변경 기능 추가

--- src/components/rank/rank.tsx
+++ src/components/rank/rank.tsx
@@ -6,25 +6,30 @@ import { useGetTeamRankList } from '@/api/league';
 import { LoadingBox } from '@/components/common/loading-spinner';
 import useLeagueName from '@/hooks/useLeagueName';
 import { leagueIdMapper } from '@/utils/league-mapper';
+import useLeagueSeason from '@/hooks/use-league-season';
 
 export default function Rank() {
   // Todo: 현재 2023 팀랭크만 구현되어 2023으로 임시 설정
-  // const { season } = useLeagueSeason();
-  // const fullSeason = '20' + season;
-  const fullSeason = 2023;
+  const { season, setSeason } = useLeagueSeason();
+  const fullSeason = '20' + season;
+  // const fullSeason = 2023;
   const leagueId = leagueIdMapper[useLeagueName() as keyof typeof leagueIdMapper];
   const { isPending, data, error } = useGetTeamRankList({
     season: fullSeason.toString(),
     leagueId,
   });
 
+  function handleSelectSeason(year: number) {
+    setSeason((prevState) => prevState + year);
+  }
+
   return (
     <>
-      {/*<RankHeader season={season} />*/}
-      <RankHeader season={23} />
+      <RankHeader onSelectSeason={handleSelectSeason} season={season} />
+      {/*<RankHeader season={23} />*/}
       {isPending ? <LoadingBox /> : null}
       {error ? <div>데이터가져오기 실패</div> : null}
-      {data ? <RankTable ranks={data} /> : <div>데이터가 없습니다.</div>}
+      {data ? <RankTable ranks={data} /> : null}
     </>
   );
 }
