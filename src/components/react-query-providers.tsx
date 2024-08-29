2b916be25289a4274bf494ece58f686e41750744
Chuhj <chj9574@naver.com>
2024-08-30 01:59:21 +0900

    Feat: queryClient 분리, 옵션 설정

--- src/components/react-query-providers.tsx
+++ src/components/react-query-providers.tsx
@@ -1,22 +1,10 @@
 'use client';
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from '@/api/query-client';
 
 export default function ReactQueryProviders({ children }: React.PropsWithChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
-          },
-        },
-      }),
-  );
-
   return (
     <QueryClientProvider client={queryClient}>
       {children}
