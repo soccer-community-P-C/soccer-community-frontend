e9ed035e81567630c208c154f84569cfd27df3c5
PARKmaker <tkdgus8903@gmail.com>
2024-09-02 23:56:40 +0900

    Style: input, button border색 변경

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,31 +1,3 @@
-// import { ComponentPropsWithoutRef, ReactNode } from 'react';
-// import { twMerge } from 'tailwind-merge';
-//
-// type ButtonProps = {
-//   children: ReactNode;
-//   className?: string;
-// } & ComponentPropsWithoutRef<'button'>;
-// export default function Button({ children, className = '', ...otherProps }: ButtonProps) {
-//   return (
-//     //focus:outline-none focus:ring-4 focus:ring-gray-100
-//     <button
-//       className={twMerge(
-//         `
-//         h-9 justify-center whitespace-nowrap rounded-md border border-gray-300 bg-white px-4 py-2
-//         text-sm font-medium text-gray-900 shadow transition-colors hover:bg-gray-100
-//         disabled:pointer-events-none disabled:opacity-50 dark:border-gray-600 dark:bg-gray-800
-//       dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700
-//        `,
-//         className,
-//       )}
-//       type="button"
-//       {...otherProps}
-//     >
-//       {children}
-//     </button>
-//   );
-// }
-
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
@@ -33,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/cn';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium border-gray-300  ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,34 @@
+// import React from 'react';
+// import { twMerge } from 'tailwind-merge';
+//
+// type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+//
+// const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
+//   { className, ...props }: InputProps,
+//   ref,
+// ) {
+//   return (
+//     <input
+//       className={twMerge(
+//         `
+//         w-full rounded-md border border-gray-300 px-3 py-2
+//         focus-visible:outline-none focus-visible:ring-2
+//         focus-visible:ring-black focus-visible:ring-offset-2
+//         disabled:cursor-not-allowed disabled:opacity-50
+//        `,
+//         className,
+//       )}
+//       ref={ref}
+//       {...props}
+//     />
+//   );
+// });
+//
+// export default Input;
+
 import * as React from 'react';
 import { cn } from '@/utils/cn';
+
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
@@ -7,7 +36,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-10 w-full rounded-md border border-gray-300 border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
