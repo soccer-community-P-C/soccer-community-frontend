67b4791daec0c1882b825b749fd251b11b172b0c
Chuhj <chj9574@naver.com>
2024-09-03 06:48:11 +0900

    Feat: withAuth 구현

---
+++ src/hocs/withAuth.tsx
@@ -0,0 +1,22 @@
+import { redirect } from 'next/navigation';
+import { useAuth } from '@/contexts/auth-context';
+
+export default function withAuth<Props extends React.JSX.IntrinsicAttributes>(
+  WrappedComponent: React.ComponentType<Props>,
+) {
+  function AuthenticatedComponent(props: Props) {
+    const { isAuthenticated } = useAuth();
+
+    if (isAuthenticated) {
+      return <WrappedComponent {...props} />;
+    } else {
+      redirect('/login');
+      return null;
+    }
+  }
+
+  const wrappedComponentName = WrappedComponent.displayName || WrappedComponent.name || 'Component';
+  AuthenticatedComponent.displayName = `withAuth(${wrappedComponentName})`;
+
+  return AuthenticatedComponent;
+}
