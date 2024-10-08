e905179559fc4b4d91db10741458439edac55cf1
PARKmaker <tkdgus8903@gmail.com>
2024-10-08 21:01:58 +0900

    Style: table padding 값 수정

--- src/components/match/lineup/lineup-table-tbody.tsx
+++ src/components/match/lineup/lineup-table-tbody.tsx
@@ -1,8 +1,4 @@
-import Link from 'next/link';
-import Card from '@/components/match/card/card';
 import { TGameBooking, TGamePlayer } from '@/types/schedules';
-import { splitName } from '@/components/match/match.utils';
-import ChangeIcon from '@/components/match/lineup/change-icon';
 import LineupPlayer from '@/components/match/lineup/lineup-player';
 
 type LineupTableTbodyProps = {
@@ -21,7 +17,7 @@ export default function LineupTableTbody({ homeLineup, awayLineup }: LineupTable
           return (
             <tr className="border-b" key={index}>
               <td>
-                <div className="border-r pb-1 pl-1 pt-4 sm:pl-16">
+                <div className="border-r pl-1 pt-1 sm:pb-1 sm:pl-16 sm:pt-4">
                   <LineupPlayer
                     cardType={homePlayer.cardType}
                     name={homePlayer.playerKrName}
@@ -32,7 +28,7 @@ export default function LineupTableTbody({ homeLineup, awayLineup }: LineupTable
                 </div>
               </td>
               <td>
-                <div className="py-4 pl-2 sm:pl-16">
+                <div className="pl-1 pt-1 sm:pb-1 sm:pl-16 sm:pt-4">
                   <LineupPlayer
                     cardType={awayPlayer.cardType}
                     name={awayPlayer.playerKrName}
@@ -42,6 +38,28 @@ export default function LineupTableTbody({ homeLineup, awayLineup }: LineupTable
                   />
                 </div>
               </td>
+              {/*<td>*/}
+              {/*  <div className="border-r pl-1 pt-1 sm:pb-1 sm:pl-16 sm:pt-4">*/}
+              {/*    <LineupPlayer*/}
+              {/*      cardType={homePlayer.cardType}*/}
+              {/*      name={homePlayer.playerKrName}*/}
+              {/*      playerId={homePlayer.playerId}*/}
+              {/*      position={homePlayer.position}*/}
+              {/*      shirtNumber={homePlayer.shirtNumber}*/}
+              {/*    />*/}
+              {/*  </div>*/}
+              {/*</td>*/}
+              {/*<td>*/}
+              {/*  <div className="pl-1 pt-1 sm:pb-1 sm:pl-16 sm:pt-4">*/}
+              {/*    <LineupPlayer*/}
+              {/*      cardType={awayPlayer.cardType}*/}
+              {/*      name={awayPlayer.playerKrName}*/}
+              {/*      playerId={awayPlayer.playerId}*/}
+              {/*      position={awayPlayer.position}*/}
+              {/*      shirtNumber={awayPlayer.shirtNumber}*/}
+              {/*    />*/}
+              {/*  </div>*/}
+              {/*</td>*/}
             </tr>
           );
         })}
--- src/components/match/lineup/lineup-table-thead.tsx
+++ src/components/match/lineup/lineup-table-thead.tsx
@@ -10,13 +10,13 @@ export default function LineupTableThead({ homeLogo, awayLogo }: LineupTableThea
     <thead>
       <tr>
         <th>
-          <div className="flex-all-center h-[40px] text-lg font-semibold sm:pl-[40px]">
+          <div className="flex h-[40px] items-center justify-start text-lg font-semibold sm:pl-16">
             <Image alt="홈로고" className="h-10 w-10" height={24} src={homeLogo} width={24} />
             <span className="pl-2">홈</span>
           </div>
         </th>
         <th>
-          <div className="flex-all-center h-[40px] text-lg font-semibold sm:pl-[40px]">
+          <div className="flex h-[40px] items-center justify-start text-lg font-semibold sm:pl-16">
             <Image alt="원정로고" className="h-10 w-10" height={24} src={awayLogo} width={24} />
             <span>원정</span>
           </div>
