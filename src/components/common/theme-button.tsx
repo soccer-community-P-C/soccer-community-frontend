06beb18ef80953a3768d08c5b296ff1dc5e9784d
PARKmaker <tkdgus8903@gmail.com>
2024-10-08 21:53:26 +0900

    Style: 헤더 다크모드 토글 버튼 수정

--- src/components/common/theme-button.tsx
+++ src/components/common/theme-button.tsx
@@ -6,15 +6,23 @@ export default function ThemeButton() {
   const { setTheme } = useTheme();
 
   return (
-    <Button className="ring-0 ring-offset-0 focus-visible:ring-0" size="icon" variant="ghost">
-      <Sun
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    <>
+      <Button
+        className="absolute rotate-90 scale-0 ring-0 ring-offset-0 transition-all focus-visible:ring-0 dark:rotate-0 dark:scale-100"
         onClick={() => setTheme('right')}
-      />
-      <Moon
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        size="icon"
+        variant="ghost"
+      >
+        <Sun className="absolute h-[1.2rem] w-[1.2rem]" />
+      </Button>
+      <Button
+        className="rotate-0 scale-100 ring-0 ring-offset-0 transition-all focus-visible:ring-0 dark:-rotate-90 dark:scale-0"
         onClick={() => setTheme('dark')}
-      />
-    </Button>
+        size="icon"
+        variant="ghost"
+      >
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      </Button>
+    </>
   );
 }
