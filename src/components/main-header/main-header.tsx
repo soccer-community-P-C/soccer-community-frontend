875320599897b6ad2eb0cb8e9df93e9583405352
PARKmaker <tkdgus8903@gmail.com>
2024-10-10 00:52:00 +0900

    Style: z-index 값 수정

--- src/components/main-header/main-header.tsx
+++ src/components/main-header/main-header.tsx
@@ -2,7 +2,7 @@ import MainHeaderNav from '@/components/main-header/main-header-nav';
 
 export default function MainHeader() {
   return (
-    <header className="sticky top-0 z-[1000] h-[4rem] w-full min-w-mobile bg-gray-600 duration-500">
+    <header className="sticky top-0 z-50 h-[4rem] w-full min-w-mobile bg-gray-600 duration-500">
       <MainHeaderNav />
     </header>
   );
